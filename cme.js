77fed360ee52fd5a44778c398c4c21cf1869b70a
utsaga11 <utsaga22@gmail.com>
2021-12-02 23:02:02 +0530

    catch me if u can

---
+++ cme.js
@@ -0,0 +1,30 @@
+var box = document.getElementById('box');
+
+var viewWidth = window.innerWidth;
+var viewHeight= window.innerHeight;
+
+window.addEventListener("resize", function(event){
+    viewWidth = window.innerWidth;
+    viewHeight= window.innerHeight;
+});
+
+box.addEventListener("mouseover", function(event){
+    var boxAttr = box.getBoundingClientRect();
+
+    var pos = getNewPosition(boxAttr.width, boxAttr.height);
+
+    box.style.top = pos.y + "px";
+    box.style.left = pos.x + "px";
+});
+
+function getNewPosition(boxWidth, boxHeight){
+    var newX = Math.floor((Math.random()*viewWidth) + 1 - boxWidth);
+    var newY = Math.floor((Math.random()*viewHeight) + 1 - boxHeight);
+    if(newX < 0){
+        newX = 0;
+    }
+    if( newY < 0){
+        newY = 0;
+    }
+    return {x:newX, y:newY};
+}
